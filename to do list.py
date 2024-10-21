b103684cbf6f03fab7ace109a7b7fe3770803731
shaan-0809 <2k22.csaiml.2213469@gmail.com>
2024-10-21 12:52:11 +0530

    Add files via upload

---
+++ to do list.py
@@ -0,0 +1,51 @@
+class TodoList:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+        print(f'Task "{task}" added.')
+
+    def remove_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+            print(f'Task "{task}" removed.')
+        else:
+            print(f'Task "{task}" not found.')
+
+    def view_tasks(self):
+        if not self.tasks:
+            print("Your to-do list is empty.")
+        else:
+            print("To-Do List:")
+            for i, task in enumerate(self.tasks, start=1):
+                print(f"{i}. {task}")
+
+def main():
+    todo_list = TodoList()
+
+    while True:
+        print("\nOptions:")
+        print("1. Add Task")
+        print("2. Remove Task")
+        print("3. View Tasks")
+        print("4. Exit")
+        
+        choice = input("Choose an option: ")
+        
+        if choice == '1':
+            task = input("Enter a task: ")
+            todo_list.add_task(task)
+        elif choice == '2':
+            task = input("Enter a task to remove: ")
+            todo_list.remove_task(task)
+        elif choice == '3':
+            todo_list.view_tasks()
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option, please try again.")
+
+if __name__ == "__main__":
+    main()
